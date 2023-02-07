d5486edded890c41c8b2a9a410164cbbd605c40e
kundan571 <kundanchauhan1997@gmail.com>
2023-02-07 13:04:28 +0530

    Added: perfect number

---
+++ src/logicalprogram/Perfectnumber.java
@@ -0,0 +1,34 @@
+package logicalprogram;
+
+public class Perfectnumber {
+    static boolean isPerfect(int number)
+    {
+        // To store sum of divisors
+        int divisorsum = 0;
+
+        // Find all divisors and add them
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0) {
+                System.out.println(i + " ");
+                divisorsum = divisorsum + i;
+            }
+        }
+        if (divisorsum == number){
+            return true;
+        }
+      else
+        return false;
+    }
+    public static void main (String[] args)
+    {
+
+        int number = 4;
+           boolean check = isPerfect(6);
+           if (check == true)
+                System.out.println( number + " is a perfect number");
+           else {
+               System.out.println(number + " Not a perfect number");
+           }
+    }
+}
