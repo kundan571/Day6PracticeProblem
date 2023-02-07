da48ba1b8b591c1c90ef82fd1bb38e5f02cd963f
kundan571 <kundanchauhan1997@gmail.com>
2023-02-07 13:10:03 +0530

    Changing inditation

--- src/logicalprogram/Perfectnumber.java
+++ src/logicalprogram/Perfectnumber.java
@@ -1,34 +1,28 @@
 package logicalprogram;
 
 public class Perfectnumber {
-    static boolean isPerfect(int number)
-    {
+    static boolean isPerfect(int number) {
         // To store sum of divisors
         int divisorsum = 0;
-
         // Find all divisors and add them
-        for (int i = 1; i < number; i++)
-        {
+        for (int i = 1; i < number; i++) {
             if (number % i == 0) {
                 System.out.println(i + " ");
                 divisorsum = divisorsum + i;
             }
         }
-        if (divisorsum == number){
+        if (divisorsum == number) {
             return true;
-        }
-      else
-        return false;
+        } else
+            return false;
     }
-    public static void main (String[] args)
-    {
-
+    public static void main(String[] args) {
         int number = 4;
-           boolean check = isPerfect(6);
-           if (check == true)
-                System.out.println( number + " is a perfect number");
-           else {
-               System.out.println(number + " Not a perfect number");
-           }
+        boolean check = isPerfect(6);
+        if (check == true)
+            System.out.println(number + " is a perfect number");
+        else {
+            System.out.println(number + " Not a perfect number");
+        }
     }
 }
